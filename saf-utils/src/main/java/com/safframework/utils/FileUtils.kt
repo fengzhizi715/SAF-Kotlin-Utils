686a31091020a33a8c50c237d074790ae8cc3162
fengzhizi715 <fengzhizi715@126.com>
2017-02-27 13:02:03 +0800

    完善FileUtils.kt

--- saf-utils/src/main/java/com/safframework/utils/FileUtils.kt
+++ saf-utils/src/main/java/com/safframework/utils/FileUtils.kt
@@ -1,15 +1,14 @@
 package com.safframework.utils
 
 import java.io.File
+import java.io.IOException
 
 /**
  * Created by Tony Shen on 2017/1/20.
  */
 object FileUtils {
 
-    @JvmStatic fun exists(file: File?): Boolean {
-        return file != null && file.exists()
-    }
+    @JvmStatic fun exists(file: File?): Boolean = file != null && file.exists()
 
     /**
      * 判断是否文件
@@ -26,4 +25,34 @@ object FileUtils {
      * @return
      */
     @JvmStatic fun isDirectory(file: File) : Boolean = exists(file) && file.isDirectory
+
+    /**
+     * 判断目录是否存在，不存在则判断是否创建成功
+     *
+     * @param file 文件
+     * @return boolean
+     */
+    @JvmStatic fun createOrExistsDir(file: File?): Boolean
+            = (file != null) && if (file.exists()) file.isDirectory else file.mkdirs()
+
+    /**
+     * 判断文件是否存在，不存在则判断是否创建成功
+     *
+     * @param file 文件
+     * @return boolean
+     */
+    @JvmStatic fun createOrExistsFile(file: File?): Boolean {
+        if (file == null || !createOrExistsDir(file.parentFile)) {
+            return false
+        }
+        if (file.exists()) {
+            return file.isFile
+        }
+        try {
+            return file.createNewFile()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return false
+        }
+    }
 }
