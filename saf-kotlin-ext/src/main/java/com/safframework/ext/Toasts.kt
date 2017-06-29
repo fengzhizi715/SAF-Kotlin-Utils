fcfcde7a05dce469a526834e052eb0251e3469ee
fengzhizi715 <fengzhizi715@126.com>
2017-06-29 23:58:52 +0800

    update Toasts.kt

--- saf-kotlin-ext/src/main/java/com/safframework/ext/Toasts.kt
+++ saf-kotlin-ext/src/main/java/com/safframework/ext/Toasts.kt
@@ -7,6 +7,8 @@ import android.widget.Toast
  * Created by Tony Shen on 2017/6/29.
  */
 
-fun Context.showToast(string: String) {
-    Toast.makeText(this, string, Toast.LENGTH_SHORT).show()
-}
+fun Context.showToast(string: String) = showToast(string,Toast.LENGTH_SHORT)
+
+fun Context.showToast(string: String,duration: Int) = Toast.makeText(this, string, duration).show()
+
+fun Context.showLongToast(string: String) = Toast.makeText(this, string, Toast.LENGTH_LONG).show()
