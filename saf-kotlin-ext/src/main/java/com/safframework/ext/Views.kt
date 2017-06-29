5b5fa11e9c8e19e7586ede1abc681de27f105460
fengzhizi715 <fengzhizi715@126.com>
2017-06-30 01:23:44 +0800

    update Views.kt

--- saf-kotlin-ext/src/main/java/com/safframework/ext/Views.kt
+++ saf-kotlin-ext/src/main/java/com/safframework/ext/Views.kt
@@ -1,6 +1,8 @@
 package com.safframework.ext
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
@@ -16,4 +18,28 @@ fun View.hideKeyboard() {
 fun View.showKeyboard() {
     requestFocus()
     (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+}
+
+fun View.toBitmap(): Bitmap?{
+    clearFocus()
+    isPressed = false
+    val willNotCache = willNotCacheDrawing()
+    setWillNotCacheDrawing(false)
+    // Reset the drawing cache background color to fully transparent
+    // for the duration of this operation
+    val color = drawingCacheBackgroundColor
+    drawingCacheBackgroundColor = 0
+    if (color != 0) destroyDrawingCache()
+    buildDrawingCache()
+    val cacheBitmap = drawingCache
+    if (cacheBitmap == null) {
+        Log.e("Views", "failed to get bitmap from $this", RuntimeException())
+        return null
+    }
+    val bitmap = Bitmap.createBitmap(cacheBitmap)
+    // Restore the view
+    destroyDrawingCache()
+    setWillNotCacheDrawing(willNotCache)
+    drawingCacheBackgroundColor = color
+    return bitmap
 }
