06baed6cae84a9bb8f84674a3bf35bccf6f5c415
fengzhizi715 <fengzhizi715@126.com>
2018-04-17 17:56:25 +0800

    update Contexts.kt

--- saf-kotlin-ext/src/main/java/com/safframework/ext/Contexts.kt
+++ saf-kotlin-ext/src/main/java/com/safframework/ext/Contexts.kt
@@ -3,7 +3,11 @@ package com.safframework.ext
 import android.content.Context
 import android.content.pm.PackageManager
 import android.support.annotation.ColorRes
+import android.support.annotation.LayoutRes
 import android.support.annotation.StringRes
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 /**
  * Created by Tony Shen on 2017/6/30.
@@ -12,6 +16,9 @@ import android.support.annotation.StringRes
 fun Context.string(@StringRes id: Int): String = getString(id)
 
 fun Context.color(@ColorRes id: Int): Int = resources.getColor(id)
+
+fun Context.inflateLayout(@LayoutRes layoutId: Int, parent: ViewGroup? = null, attachToRoot: Boolean = false): View
+        = LayoutInflater.from(this).inflate(layoutId, parent, attachToRoot)
 
 /**
  * 获取当前app的版本号
