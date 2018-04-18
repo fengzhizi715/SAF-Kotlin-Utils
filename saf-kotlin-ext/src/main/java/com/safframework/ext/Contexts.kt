bc5b35443504b066cb12fdc29eec57a7faef6868
fengzhizi715 <fengzhizi715@126.com>
2018-04-18 13:03:40 +0800

    update Contexts.kt

--- saf-kotlin-ext/src/main/java/com/safframework/ext/Contexts.kt
+++ saf-kotlin-ext/src/main/java/com/safframework/ext/Contexts.kt
@@ -30,9 +30,9 @@ val Context.screenHeight
  * returns dip(dp) dimension value in pixels
  * @param value dp
  */
-fun Context.dip2px(value: Int): Int = (value * resources.displayMetrics.density).toInt()
+fun Context.dp2px(value: Int): Int = (value * resources.displayMetrics.density).toInt()
 
-fun Context.dip2px(value: Float): Int = (value * resources.displayMetrics.density).toInt()
+fun Context.dp2px(value: Float): Int = (value * resources.displayMetrics.density).toInt()
 
 /**
  * return sp dimension value in pixels
@@ -46,7 +46,7 @@ fun Context.sp2px(value: Float): Int = (value * resources.displayMetrics.scaledD
  * converts [px] value into dip or sp
  * @param px
  */
-fun Context.px2dip(px: Int): Float = px.toFloat() / resources.displayMetrics.density
+fun Context.px2dp(px: Int): Float = px.toFloat() / resources.displayMetrics.density
 
 fun Context.px2sp(px: Int): Float = px.toFloat() / resources.displayMetrics.scaledDensity
 
