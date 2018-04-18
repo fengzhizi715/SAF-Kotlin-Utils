f75fdfd08fd3fe70cef417824910e604dbd454f9
fengzhizi715 <fengzhizi715@126.com>
2018-04-18 11:03:07 +0800

    update Contexts.kt

--- saf-kotlin-ext/src/main/java/com/safframework/ext/Contexts.kt
+++ saf-kotlin-ext/src/main/java/com/safframework/ext/Contexts.kt
@@ -3,6 +3,7 @@ package com.safframework.ext
 import android.content.Context
 import android.content.pm.PackageManager
 import android.support.annotation.ColorRes
+import android.support.annotation.DimenRes
 import android.support.annotation.LayoutRes
 import android.support.annotation.StringRes
 import android.view.LayoutInflater
@@ -12,6 +13,49 @@ import android.view.ViewGroup
 /**
  * Created by Tony Shen on 2017/6/30.
  */
+
+/**
+ * screen width in pixels
+ */
+val Context.screenWidth
+    get() = resources.displayMetrics.widthPixels
+
+/**
+ * screen height in pixels
+ */
+val Context.screenHeight
+    get() = resources.displayMetrics.heightPixels
+
+/**
+ * returns dip(dp) dimension value in pixels
+ * @param value dp
+ */
+fun Context.dip2px(value: Int): Int = (value * resources.displayMetrics.density).toInt()
+
+fun Context.dip2px(value: Float): Int = (value * resources.displayMetrics.density).toInt()
+
+/**
+ * return sp dimension value in pixels
+ * @param value sp
+ */
+fun Context.sp2px(value: Int): Int = (value * resources.displayMetrics.scaledDensity).toInt()
+
+fun Context.sp2px(value: Float): Int = (value * resources.displayMetrics.scaledDensity).toInt()
+
+/**
+ * converts [px] value into dip or sp
+ * @param px
+ */
+fun Context.px2dip(px: Int): Float = px.toFloat() / resources.displayMetrics.density
+
+fun Context.px2sp(px: Int): Float = px.toFloat() / resources.displayMetrics.scaledDensity
+
+/**
+ * return dimen resource value in pixels
+ * @param resource dimen resource
+ */
+fun Context.dimen2px(@DimenRes resource: Int): Int = resources.getDimensionPixelSize(resource)
+
 
 fun Context.string(@StringRes id: Int): String = getString(id)
 
