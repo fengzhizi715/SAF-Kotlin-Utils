bdaa4139a92da6e5e0935c65864c4e2665d685ed
fengzhizi715 <fengzhizi715@126.com>
2018-04-22 22:55:22 +0800

    update Views.kt

--- saf-kotlin-ext/src/main/java/com/safframework/ext/Views.kt
+++ saf-kotlin-ext/src/main/java/com/safframework/ext/Views.kt
@@ -5,7 +5,6 @@ import android.graphics.Bitmap
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import com.safframework.ext.utils.Utils
 
 /**
  * Created by Tony Shen on 2017/6/30.
@@ -28,11 +27,63 @@ fun View.showKeyboard() {
     (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
 }
 
+/***
+ * 设置延迟时间的View扩展
+ * @param delay Long 延迟时间，默认600毫秒
+ * @return T
+ */
+fun <T : View> T.withTrigger(delay: Long = 600): T {
+    triggerLastTime = delay
+    return this
+}
+
+/***
+ * 点击事件的View扩展
+ * @param block: (T) -> Unit 函数
+ * @return Unit
+ */
 fun <T : View> T.click(block: (T) -> Unit) = setOnClickListener {
 
-    if (!Utils.isFastClick()) {
+    if (clickEnable()) {
         block(it as T)
     }
+}
+
+/***
+ * 带延迟过滤的点击事件View扩展
+ * @param delay Long 延迟时间，默认600毫秒
+ * @param block: (T) -> Unit 函数
+ * @return Unit
+ */
+fun <T : View> T.clickWithTrigger(time: Long = 600, block: (T) -> Unit) = {
+    triggerDelay = time
+    setOnClickListener {
+        if (clickEnable()) {
+            block(it as T)
+        }
+    }
+}
+
+private var <T : View> T.triggerLastTime: Long
+    get() = if (getTag(1123460103) != null) getTag(1123460103) as Long else 0
+    set(value) {
+        setTag(1123460103, value)
+    }
+
+private var <T : View> T.triggerDelay: Long
+    get() = if (getTag(1123461123) != null) getTag(1123461123) as Long else -1
+    set(value) {
+        setTag(1123461123, value)
+    }
+
+private fun <T : View> T.clickEnable(): Boolean {
+    var flag = false
+    val currentClickTime = System.currentTimeMillis()
+    if (currentClickTime - triggerLastTime >= triggerDelay) {
+        flag = true
+    }
+    triggerLastTime = currentClickTime
+    return flag
 }
 
 fun <T : View> T.longClick(block: (T) -> Boolean) = setOnLongClickListener { block(it as T) }
