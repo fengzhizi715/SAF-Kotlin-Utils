d1690eea7a14a073012262a8ee2723858c344800
fengzhizi715 <fengzhizi715@126.com>
2018-04-23 19:42:03 +0800

    update Booleans.kt

--- saf-kotlin-ext/src/main/java/com/safframework/ext/Booleans.kt
+++ saf-kotlin-ext/src/main/java/com/safframework/ext/Booleans.kt
@@ -9,3 +9,9 @@ package com.safframework.ext
  * @version V1.0 <描述当前版本功能>
  */
 infix fun <T> Boolean.then(value: T?) = if (this) value else null
+
+inline fun <T> Boolean.then(function: () -> T, default: () -> T) = if (this) function() else default()
+
+infix inline fun <reified T> Boolean.then(function: () -> T) = if (this) function() else null
+
+infix inline fun <reified T, reified Type> Type?.then(block: (Type) -> T) = if (this != null) block(this) else null
