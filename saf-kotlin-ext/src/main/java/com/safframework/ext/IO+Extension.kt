0e711038f78e100fdf09b6d895c51989bcc36d38
fengzhizi715 <fengzhizi715@126.com>
2018-05-01 23:04:17 +0800

    update IO+Extension.kt

--- saf-kotlin-ext/src/main/java/com/safframework/ext/IO+Extension.kt
+++ saf-kotlin-ext/src/main/java/com/safframework/ext/IO+Extension.kt
@@ -18,3 +18,21 @@ fun Closeable?.closeQuietly() {
         // ignore
     }
 }
+
+inline fun <T : Closeable?, R> T.use(block: (T) -> R): R {
+    var closed = false
+    try {
+        return block(this)
+    } catch (e: Exception) {
+        closed = true
+        try {
+            this?.close()
+        } catch (closeException: Exception) {
+        }
+        throw e
+    } finally {
+        if (!closed) {
+            this?.close()
+        }
+    }
+}
