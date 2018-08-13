5595358a7ed3b7b9bdcf5b22b50c6fec677aafff
fengzhizi715 <fengzhizi715@126.com>
2018-08-13 20:53:46 +0800

    update Boolean+Extension.kt

--- saf-kotlin-ext/src/main/java/com/safframework/ext/Boolean+Extension.kt
+++ saf-kotlin-ext/src/main/java/com/safframework/ext/Boolean+Extension.kt
@@ -10,6 +10,8 @@ package com.safframework.ext
  */
 infix fun <T> Boolean.then(value: T?) = if (this) value else null
 
+fun <T> Boolean.then(value: T?, default: T) = if (this) value else default
+
 inline fun <T> Boolean.then(function: () -> T, default: T) = if (this) function() else default
 
 inline fun <T> Boolean.then(function: () -> T, default: () -> T) = if (this) function() else default()
